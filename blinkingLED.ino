0e686f1ea0d8e36632f9804672922506596c1ec5
AhmedShahan <shahan.ahmed001@gmail.com>
2023-07-31 14:00:54 +0600

    Blinking LED

---
+++ blinkingLED.ino
@@ -0,0 +1,21 @@
+
+// Version 1
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+int main()
+{
+  digitalWrite(13, HIGH);
+}
+
+
+// Version 2
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(13, HIGH);
+}
