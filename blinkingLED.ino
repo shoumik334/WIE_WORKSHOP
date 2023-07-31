1cd45369de361596545ded2fcd8691e0e1d1ed27
AhmedShahan <shahan.ahmed001@gmail.com>
2023-07-31 14:03:33 +0600

    minor changes

--- blinkingLED.ino
+++ blinkingLED.ino
@@ -2,20 +2,20 @@
 // Version 1
 void setup()
 {
-  pinMode(13, OUTPUT);
+ pinMode(13, OUTPUT);
 }
 int main()
 {
-  digitalWrite(13, HIGH);
+ digitalWrite(13, HIGH);
 }
 
 
 // Version 2
 void setup()
 {
-  pinMode(13, OUTPUT);
+ pinMode(13, OUTPUT);
 }
 void loop()
 {
-  digitalWrite(13, HIGH);
+ digitalWrite(13, HIGH);
 }
