379bd6db5c6ab9a1344f61043a6398c173c3c2da
AhmedShahan <shahan.ahmed001@gmail.com>
2023-07-31 19:57:25 +0600

    final uploaded

--- final_project.ino
+++ final_project.ino
@@ -1,34 +1,32 @@
-// Version 1
-void setup()
+pinMode(A0, INPUT);
+    pinMode(16, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(4, OUTPUT);
+    Serial.begin(9600);
+}void loop()
 {
-  pinMode(A0, INPUT);
-  pinMode(16, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-  Serial.begin(9600);
-}
-void loop()
+    float value=analogRead(A0);
+    Serial.println(value);
+    float endValue=1025;
+if (value>= 0 && value<=200)
 {
-  float value=analogRead(A0);
-  Serial.println(value);
-  float endValue=1025;
-  if (value>= 0 && value<=200)
-  {
-    Serial.println("Yellow");
-    digitalWrite(16, HIGH);
-  }
-  else if (value>= 201 && value<=900)
-  {
-    Serial.println("Green");
-    digitalWrite(5, HIGH);
-  }
-  else
-  {
-    Serial.println("Red");
-    digitalWrite(4, HIGH);
-  }
-  delay(100);
+    Serial.println("Yellow");
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(16, HIGH);
 }
-
-
-// version 2
+else if (value>= 201 && value<=900)
+{
+    Serial.println("Green");
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(16, LOW);
+}
+else
+{
+    Serial.println("Red");
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(16, LOW);}
+delay(100);
+}
